0b63df59985ecf718b0921146104b01afc789df4
n5upi <dallasats@hotmail.com>
2024-11-27 04:53:28 -0500

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,17 +5,22 @@ from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
+    # path for registration
+    path(route='register', view=views.registration, name='register'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
     # path for logout
-    #path(route='logout', view=views.logout_user, name='logout'),
-
+    path('logout/', views.logout_request, name='logout')
+    # path(route='logout', view=views.logout_request, name='logout'),
+ 
     # path for dealer reviews view
+    #path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
+    #path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
