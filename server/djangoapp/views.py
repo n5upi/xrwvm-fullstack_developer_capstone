43c6c70f5ed7410e0b746ea369efbf9dc1f017bb
n5upi <dallasats@hotmail.com>
2024-12-10 05:09:23 -0500

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -97,12 +97,7 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse(
-        {
-            "status": 200,
-            "dealers": dealerships
-        }
-    )
+    return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
@@ -114,19 +109,9 @@ def get_dealer_reviews(request, dealer_id):
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
             review_detail['sentiment'] = response['sentiment']
-        return JsonResponse(
-            {
-                "status": 200,
-                "reviews": reviews
-            }
-        )
+        return JsonResponse({"status":200,"reviews":reviews})
     else:
-        return JsonResponse(
-            {
-                "status": 400,
-                "message": "Bad Request"
-            }
-        )
+         return JsonResponse({"status":400,"message":"Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
@@ -140,12 +125,7 @@ def get_dealer_details(request, dealer_id):
             }
         )
     else:
-        return JsonResponse(
-            {
-                "status": 400,
-                "message": "Bad Request"
-            }
-        )
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 # Create a `add_review` view to submit a review
 def add_review(request):
